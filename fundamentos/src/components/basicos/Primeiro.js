9ea7900da7aa047af524f8fbbf1bab76937045a4
João Paulo <joaopaulomirandamatias@users.noreply.github.com>
2021-02-27 13:42:07 -0300

    Curso

---
+++ fundamentos/src/components/basicos/Primeiro.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Primeiro(){
+    return (
+        <di>
+            <h2>Melinda do Papai</h2>
+            <h3>Bebê dengosa</h3>
+        </di>
+    )
+    
+}
