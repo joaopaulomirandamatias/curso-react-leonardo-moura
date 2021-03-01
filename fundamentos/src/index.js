f8442ed5699e310ff630d00d357dbd427191bfb9
João Paulo <37987074+joaopaulomirandamatias@users.noreply.github.com>
2021-03-01 15:25:39 -0300

    Aula 13 Componente com propriedade

--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -2,11 +2,17 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import './index.css'
 import Primeiro from './components/basicos/Primeiro'
-
+import ComParamentro from './components/basicos/ComParametro'
 
 ReactDOM.render(
-    <div>
+    <div id="app">
         <Primeiro></Primeiro>
+        <ComParamentro 
+            titulo="Situação do Aluno"
+            aluno="João Paulo"
+            nota={8.3}
+        />
+            
     </div>, 
     document.getElementById('root')
 )
