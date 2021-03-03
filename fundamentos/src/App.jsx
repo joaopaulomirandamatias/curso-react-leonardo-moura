800426b5a5362df59536ea472417b1ea0ccec73f
João Paulo <37987074+joaopaulomirandamatias@users.noreply.github.com>
2021-03-03 19:18:43 -0300

    Aula 22 e 23 - Componente com Filho

--- fundamentos/src/App.jsx
+++ fundamentos/src/App.jsx
@@ -5,12 +5,23 @@ import Fragmento from "./components/basicos/Fragmento";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Card from "./components/layout/Card";
 import "./App.css";
+import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
+
 
 export default (_) => (
   <div className="App">
     <h1>Fundamento React</h1>
 
     <div className="Cards">
+      <Card titulo="#04 - Componentes com Filhos" color="#00C8F8">
+        <Familia sobrenome="Matias">
+          <FamiliaMembro nome="João Paulo" />
+          <FamiliaMembro nome="Jéssica"/>
+          <FamiliaMembro nome="Melinda" />
+        </Familia>
+      </Card>
+
       <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
         <Aleatorio min={1} max={100} />
       </Card>
--- fundamentos/src/components/layout/Card.jsx
+++ fundamentos/src/components/layout/Card.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import './Card.css'
+import "./Card.css";
 
-export default (props) => {
-
+const Card = (props) => {
   const style = {
     backgroundColor: props.color || "#f00100",
-    borderColor: props.color || "#f00100"
-  }
+    borderColor: props.color || "#f00100",
+  };
   return (
-    <div className='Card' style={style}>
-      <div className='Title'>{props.titulo}</div>
-      <div className='Content'>
-        {props.children}
-      </div>
+    <div className="Card" style={style}>
+      <div className="Title">{props.titulo}</div>
+      <div className="Content">{props.children}</div>
     </div>
   );
 };
+export default Card;
